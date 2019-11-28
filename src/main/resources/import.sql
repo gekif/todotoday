6b38ff05f55385786a427dbfe0aadcaa1d84e457
Dzulfikar Maulana <dzulfikar.maulana@brainworx.co.id>
2019-11-28 12:22:02 +0700

    Users and Roles

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,3 +1,10 @@
+-- Insert trole
+INSERT INTO role (name) VALUES ('ROLE_USER');
+
+-- Insert two users
+INSERT INTO user (username, enabled, password, role_id) VALUES ('user', TRUE, 'password', 1);
+INSERT INTO user (username, enabled, password, role_id) VALUES ('user2', TRUE, 'password', 1);
+
 -- Insert tasks
 insert into task (complete,description) values (true,'Code Task entity');
 insert into task (complete,description) values (false,'Discuss users and roles');
